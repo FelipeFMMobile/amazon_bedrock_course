b73ac58b84edc12604747eab44a7502f9ce7c6e4
Felipe Menezes <fmmobile@live.com>
2024-07-14 16:16:20 -0300

    functions and results

---
+++ bedrock_code_generation.py
@@ -0,0 +1,55 @@
+import json
+import boto3
+import botocore.config
+from datetime import datetime
+
+def generate_code_using_bedrock(message: str, language: str) -> str:
+    prompt_text = f"Human: Write {language} code for the following instructions: {message}.\nAssistant:"
+    body = { 
+        "prompt": prompt_text,
+        "max_tokens_to_sample": 2048, 
+        "temperature": 0.1, 
+        "top_k": 250, 
+        "top_p": 0.2
+    }
+    print(body)
+    try:
+        bedrock = boto3.client("bedrock-runtime", region_name="us-east-1", config=botocore.config.Config(read_timeout=300, retries={'max_attempts': 3}))
+        response = bedrock.invoke_model(body=json.dumps(body), modelId="anthropic.claude-v2")
+        response_content = response.get("body").read().decode("utf-8")
+        response_data = json.loads(response_content)
+        code = response_data["completion"].strip()
+        return code
+    except Exception as e:
+        print("Error generating code:", e)
+        return ""
+
+def save_code_s3(code, s3_bucket, s3_key):
+    s3 = boto3.client('s3')
+    try:
+        s3.put_object(Bucket=s3_bucket, Key=s3_key, Body=code)
+        print("Code saved to s3")
+    except Exception as e:
+        print("Error when saving the code to s3:", e)
+
+def lambda_handler(event, context):
+    event_body = json.loads(event["body"])
+    message = event_body.get("message")
+    language = event_body.get("language")
+    print(message, language)
+    
+    generated_code = generate_code_using_bedrock(message, language)
+    
+    if generated_code:
+        current_time = datetime.now().strftime("%H%M%S")
+        s3_key = f'code-output/{current_time}.py'
+        s3_bucket = 'fmmobile-bedrock-course-bucket'
+        
+        save_code_s3(generated_code, s3_bucket, s3_key)
+    else:
+        print("No code was generated")
+    return {
+        'statusCode': 200,
+        'headers': {'Content-Type': 'application/json'},
+        'body': json.dumps('Code generation complete')
+    }
